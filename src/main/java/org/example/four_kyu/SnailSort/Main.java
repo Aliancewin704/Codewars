15ebebee9bd10ead68f17ab51ceb24ffb68c2cde
AlexRimC9 <alexrimc9@gmail.com>
2023-10-09 12:02:16 +0300

    Commit 15 Snail Sort 28.05.2024

---
+++ src/main/java/org/example/four_kyu/SnailSort/Main.java
@@ -0,0 +1,9 @@
+package org.example.four_kyu.SnailSort;
+
+
+public class Main {
+    
+    public static void main(String[] args) {
+        SnailSort.snail(new int[][]{{1,2,3},{4,5,6},{7,8,9}});
+    }
+}
